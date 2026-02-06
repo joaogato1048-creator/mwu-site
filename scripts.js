8e34f5c1c2b9c5d5c5411d67c5089d91ff1f13fd
joaogato1048-creator <joaogato1048@gmail.com>
2026-02-06 17:56:49 -0400

    subir o arquivo do meu site

---
+++ scripts.js
@@ -0,0 +1,67 @@
+const prevButton = document.getElementById('prev')
+const nexButton =  document.getElementById('next')
+const items = document.querySelectorAll('.item')
+const dots = document.querySelectorAll('.dot')
+const numberIndicator = document.querySelector('.numbers')
+const list = document.querySelector('.list')
+
+
+
+let active = 0;
+const total = items.length
+let timer;
+
+
+//clicando no botão prev  next
+
+function update(direction){
+ 
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.dot.active').classList.remove('active')
+
+    if(direction > 0){
+        active = active +1
+
+        if(active === total ){
+            active = 0
+        }
+ 
+    } 
+    
+    
+    else if(direction < 0){
+        active = active -1
+
+        if (active < 0){
+            active = total -1
+        }
+
+    }
+
+
+    items[active].classList.add('active')
+    dots[active].classList.add('active')
+
+
+
+    numberIndicator.textContent = String(active + 1).padStart(2,'0')
+}
+
+
+
+clearInterval(timer)
+time = setInterval(() =>{
+    update(1)
+
+}, 3000);
+
+
+
+prevButton.addEventListener('click', () => {
+    update(-1)
+    
+})
+
+nexButton.addEventListener('click', () => {
+    update(1)
+})
